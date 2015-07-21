517c2d3fdd379ad81e799b99d4d2791ddb0daadf
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-07-20 20:38:34 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,40 @@
+export default class {
+  constructor (win, keys) {
+    this.keys = keys
+    this.win = win
+    this.id = 'gamepad-key'
+
+    this.buttons = []
+    for (var key in this.keys) {
+      this.buttons[this.keys[key]] = {pressed: false}
+    }
+
+    this.onkey = function (event) {
+      if (event.which in this.keys) {
+        let pressed
+        if (event.type === 'keyup') {
+          pressed = true
+        } else if (event.type === 'keydown') {
+          pressed = false
+        }
+        this.buttons[this.keys[event.which]].pressed = pressed
+        this.timestamp = event.timeStamp
+        event.preventDefault()
+      }
+    }.bind(this)
+
+    this.connect()
+  }
+
+  connect () {
+    this.connected = true
+    this.win.addEventListener('keydown', this.onkey)
+    this.win.addEventListener('keyup', this.onkey)
+  }
+
+  disconnect () {
+    this.connected = false
+    this.win.removeEventListener('keydown', this.onkey)
+    this.win.removeEventListener('keyup', this.onkey)
+  }
+}
